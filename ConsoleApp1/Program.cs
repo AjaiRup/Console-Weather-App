b9b0129a4e1ef126d9581a4624356ee0ba139fe1
AjaiRup <ajairuparelia@gmail.com>
2017-09-27 19:08:34 -0700

    Initial code dump

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using GoogleMaps.LocationServices;
+using DarkSkyApi;
+using DarkSkyApi.Models;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("What is your location?");
+
+            var address = Console.ReadLine();
+            var locationService = new GoogleLocationService();
+            var point = locationService.GetLatLongFromAddress(address);
+            var latitude = point.Latitude;
+            var longitude = point.Longitude;
+
+            Console.WriteLine("Your latitude and longitude is (" + latitude.ToString() + "," + longitude.ToString() + ")" + Environment.NewLine);
+            Console.WriteLine("Press enter to view your forecast!");
+            Console.ReadLine();
+
+            using (var client = new WebClient())
+            {
+                var forecast =
+                    client.DownloadString("https://api.darksky.net/forecast/9585905bb193932089b2a674929322cf/" +
+                                          latitude + "," + longitude);
+
+                Console.WriteLine(forecast);
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
+
+
+            
